91f2eff395f46f46dcf3be419c5b63443147eb2a
Enrico Schaaf <enricoschaaf98@gmail.com>
2020-07-30 17:46:17 +0200

    stuff

--- backend/lib/zeltlager-wittlich-stack.ts
+++ backend/lib/zeltlager-wittlich-stack.ts
@@ -1,9 +1,7 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core"
 
 export class ZeltlagerWittlichStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    // The code that defines your stack goes here
+    super(scope, id, props)
   }
 }
