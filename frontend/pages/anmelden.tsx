bfbaa9418b17b0d1e04fdb570d71aef71f034a77
Enrico Schaaf <enricoschaaf98@gmail.com>
2020-08-12 20:25:28 +0200

    1. Update preact config 2. add registration page

---
+++ frontend/pages/anmelden.tsx
@@ -0,0 +1,15 @@
+import { RegistrationForm } from "components/RegistrationForm"
+import Head from "next/head"
+
+const Register = () => {
+    return (
+        <>
+            <Head>
+                <title>Anmelden | Zeltlager Wittlich</title>
+            </Head>
+            <RegistrationForm />
+        </>
+    )
+}
+
+export default Register
