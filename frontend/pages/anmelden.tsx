96125f332bc31ae5822beb7202d022f0e36f1994
Enrico Schaaf <enricoschaaf98@gmail.com>
2020-08-13 02:42:01 +0200

    create Modal component because useModal hook doesnt work with framer motion

--- frontend/pages/anmelden.tsx
+++ frontend/pages/anmelden.tsx
@@ -2,14 +2,14 @@ import { RegistrationForm } from "components/RegistrationForm"
 import Head from "next/head"
 
 const Register = () => {
-    return (
-        <>
-            <Head>
-                <title>Anmelden | Zeltlager Wittlich</title>
-            </Head>
-            <RegistrationForm />
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <title>Anmelden | Zeltlager Wittlich</title>
+      </Head>
+      <RegistrationForm />
+    </>
+  )
 }
 
 export default Register
