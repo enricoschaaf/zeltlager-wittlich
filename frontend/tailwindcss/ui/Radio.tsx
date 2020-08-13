3f4999ae46cdffdae770dfb76bee277eefa5b57a
Enrico Schaaf <enricoschaaf98@gmail.com>
2020-08-13 06:08:28 +0200

    add Radio and Checkbox components

---
+++ frontend/tailwindcss/ui/Radio.tsx
@@ -0,0 +1,34 @@
+import { DetailedHTMLProps, InputHTMLAttributes } from "react"
+
+export const Radio = ({
+  id,
+  label,
+  className,
+  register,
+  ...props
+}: RadioProps) => (
+  <div className={"flex items-center " + className}>
+    <input
+      id={id}
+      value={id}
+      type="radio"
+      className="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+      ref={register}
+      {...props}
+    />
+    <label htmlFor={id} className="ml-3">
+      <span className="block text-sm leading-5 font-medium text-gray-700">
+        {label}
+      </span>
+    </label>
+  </div>
+)
+
+interface RadioProps
+  extends DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  label: string
+  register: any
+}
--- frontend/tailwindcss/ui/index.ts
+++ frontend/tailwindcss/ui/index.ts
@@ -1,3 +1,5 @@
 export * from "./Button"
+export * from "./Checkbox"
 export * from "./Input"
 export * from "./Modal"
+export * from "./Radio"
