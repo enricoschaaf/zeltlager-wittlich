fa1a0936300b8249dd37be3fa899ba72232067fe
Enrico Schaaf <enricoschaaf98@gmail.com>
2020-08-14 20:17:55 +0200

    validation and send email

--- backend/lambda/register.ts
+++ backend/lambda/register.ts
@@ -1,13 +1,46 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda"
-import { DynamoDB } from "aws-sdk"
+import { DynamoDB, SES } from "aws-sdk"
+import * as z from "zod"
 
 const tableName = process.env.TABLE_NAME
 const year = process.env.YEAR
 const maxParticipants = process.env.MAX_PARTICIPANTS
 const dynamo = new DynamoDB.DocumentClient()
+const ses = new SES()
 
-const registerHandler: APIGatewayProxyHandlerV2 = async () => {
+const schema = z.object({
+  allergies: z.string().optional(),
+  birthDate: z.string().refine((value) => !isNaN(new Date(value).getTime())),
+  canSwim: z.boolean(),
+  city: z.string(),
+  diseases: z.string().optional(),
+  eatingHabits: z.string(),
+  email: z.string().email(),
+  familyDoctorName: z.string(),
+  familyDoctorPhone: z.string(),
+  firstName: z.string(),
+  foodIntolerances: z.string().optional(),
+  health: z.string().optional(),
+  healthInsurance: z.string(),
+  lastName: z.string(),
+  mobile: z.string(),
+  postalCode: z.string().length(5),
+  streetAddress: z.string(),
+  supervision: z.boolean(),
+})
+
+const registerHandler: APIGatewayProxyHandlerV2 = async ({ body }) => {
   try {
+    let data
+    try {
+      if (!body) throw Error
+      data = JSON.parse(body)
+      schema.parse(data)
+    } catch {
+      return {
+        statusCode: 400,
+      }
+    }
     const { Item } = await dynamo
       .get({
         TableName: tableName,
@@ -19,38 +52,62 @@ const registerHandler: APIGatewayProxyHandlerV2 = async () => {
       })
       .promise()
     const count = Item?.count ?? 0
-    await dynamo
-      .transactWrite({
-        TransactItems: [
-          {
-            Put: {
-              Item: {
-                PK: `REGISTRATIONS#${year}`,
-                SK: `REGISTRATION#${count + 1}`,
-              },
-              TableName: tableName,
-              ConditionExpression: "attribute_not_exists(SK)",
-            },
-          },
-          {
-            Update: {
-              TableName: tableName,
-              Key: {
-                PK: `REGISTRATIONS#${year}`,
-                SK: "COUNT",
-              },
-              UpdateExpression: "ADD #count :incr",
-              ExpressionAttributeNames: {
-                "#count": "count",
-              },
-              ExpressionAttributeValues: {
-                ":incr": 1,
+    await Promise.all([
+      dynamo
+        .transactWrite({
+          TransactItems: [
+            {
+              Put: {
+                Item: {
+                  PK: `REGISTRATIONS#${year}`,
+                  SK: `REGISTRATION#${count + 1}`,
+                  ...data,
+                },
+                TableName: tableName,
+                ConditionExpression: "attribute_not_exists(SK)",
               },
             },
+            {
+              Update: {
+                TableName: tableName,
+                Key: {
+                  PK: `REGISTRATIONS#${year}`,
+                  SK: "COUNT",
+                },
+                UpdateExpression: "ADD #count :incr",
+                ExpressionAttributeNames: {
+                  "#count": "count",
+                },
+                ExpressionAttributeValues: {
+                  ":incr": 1,
+                },
+              },
+            },
+          ],
+        })
+        .promise(),
+      ses
+        .sendEmail({
+          Destination: {
+            ToAddresses: [data.email],
           },
-        ],
-      })
-      .promise()
+          Message: {
+            Body: {
+              Html: {
+                Data: count < parseInt(maxParticipants) ? "success" : "waiting",
+              },
+              Text: {
+                Data: count < parseInt(maxParticipants) ? "success" : "waiting",
+              },
+            },
+            Subject: {
+              Data: count < parseInt(maxParticipants) ? "success" : "waiting",
+            },
+          },
+          Source: "Zeltlager Wittlich <noreply@zeltlager-wittlich.de>",
+        })
+        .promise(),
+    ])
     return {
       status: count < parseInt(maxParticipants) ? "success" : "waiting",
     }
