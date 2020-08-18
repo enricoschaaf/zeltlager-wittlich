6d9458815fb3fcdd30e365b9f975b8d27f363999
Enrico Schaaf <enricoschaaf98@gmail.com>
2020-08-18 21:21:34 +0200

    add layout to app component

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,11 @@
+import { Layout } from "components/Layout"
 import { AppProps } from "next/app"
 import "../styles/index.css"
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+)
 
 export default App
