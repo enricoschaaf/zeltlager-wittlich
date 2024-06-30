3f4c7fe34e36ca541cdcbf4a7259cdf4d93ca68e
Magnus Morgens <MagnusMorgens@web.de>
2024-06-30 18:33:47 +0200

    add UpdateForm

---
+++ frontend/app/anpassen/page.tsx
@@ -0,0 +1,10 @@
+import { UpdateForm } from "components/UpdateForm"
+import { formatTitle } from "utils/formatTitle"
+
+const Update = () => <UpdateForm />
+
+export const metadata = {
+  title: formatTitle("Anpassen"),
+}
+
+export default Update
